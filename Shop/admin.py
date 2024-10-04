3932048d16860047f8af300ce366d4a8c4d6903a
AlazarBekele <alazarthe70@gmail.com>
2024-10-04 20:54:12 +0300

    forget deploye admin dot python

--- Shop/admin.py
+++ Shop/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Main_upload
+from .models import Main_upload, Catagory
 
 # Register your models here.
 admin.site.register(Main_upload)
+admin.site.register(Catagory)
