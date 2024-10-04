e1f8fb003ba11516007acba3ca4742c6b79ce5c2
AlazarBekele <alazarthe70@gmail.com>
2024-10-04 20:52:42 +0300

    add catagory model

--- Shop/models.py
+++ Shop/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+class Catagory (models.Model):
+    name = models.CharField (max_length=30)
+
+    def __str__(self) -> str:
+        return self.name
+
 class Main_upload (models.Model):
 
     title = models.CharField (max_length=20)
