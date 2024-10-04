3370960b06b6d3840321bf3d711d7e452a98d2df
AlazarBekele <alazarthe70@gmail.com>
2024-10-04 19:11:49 +0300

    Delete all modes

--- Shop/admin.py
+++ Shop/admin.py
@@ -2,4 +2,3 @@ from django.contrib import admin
 from .models import News
 
 # Register your models here.
-admin.site.register(News)
--- Shop/models.py
+++ Shop/models.py
@@ -1,12 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-
-class News (models.Model):
-
-    title = models.CharField(max_length=20)
-    photo = models.ImageField(upload_to='photo/')
-
-    def __str__(self) -> str:
-        return self.title
